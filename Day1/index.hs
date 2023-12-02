30aa3137ea17cd6de1cfd2a699cdd74349fe38a8
jppianta <joaopedro.pianta@gmail.com>
2023-12-02 00:35:47 -0300

    First commit

---
+++ Day1/index.hs
@@ -0,0 +1,55 @@
+import System.IO
+import Control.Monad
+import Data.Char (digitToInt, isDigit)
+import Data.List
+
+stripFirstNumber :: String -> String
+stripFirstNumber (x:xs)
+  | isDigit x = [x]
+  | isWrittenDigit (x:xs) = writtenDigitsToDigits (stripWrittenDigit (x:xs))
+  | otherwise = stripFirstNumber xs
+
+stripLastNumber :: String -> String
+stripLastNumber x
+  | isDigit (last x) = [last x]
+  | isWrittenDigitSuffix x = writtenDigitsToDigits (stripWrittenDigitSuffix x)
+  | otherwise = stripLastNumber (last x : init x)
+
+writtenDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+writtenDigitsToDigits :: Maybe String -> String
+writtenDigitsToDigits x
+  | x == Just "one" = "1"
+  | x == Just "two" = "2"
+  | x == Just "three" = "3"
+  | x == Just "four" = "4"
+  | x == Just "five" = "5"
+  | x == Just "six" = "6"
+  | x == Just "seven" = "7"
+  | x == Just "eight" = "8"
+  | x == Just "nine" = "9"
+  | otherwise = ""
+
+isWrittenDigit :: String -> Bool
+isWrittenDigit x = any (`isPrefixOf` x) writtenDigits
+
+stripWrittenDigit :: String -> Maybe String
+stripWrittenDigit x
+  | isWrittenDigit x = find (`isPrefixOf` x) writtenDigits
+
+isWrittenDigitSuffix :: String -> Bool
+isWrittenDigitSuffix x = any (`isSuffixOf` x) writtenDigits
+
+stripWrittenDigitSuffix :: String -> Maybe String
+stripWrittenDigitSuffix x
+  | isWrittenDigitSuffix x = find (`isSuffixOf` x) writtenDigits
+
+parseLine :: String -> Int
+parseLine x = read (stripFirstNumber x ++ stripLastNumber x)
+
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+  let singlewords = words contents
+  let list = map parseLine singlewords
+  print (sum list)
